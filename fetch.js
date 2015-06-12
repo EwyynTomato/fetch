cd2f7bece35f74b26329e0904266722f10cae665
Erwin Jahja (IS-TW) <erwin_jahja@trend.com.tw>
2015-06-12 15:12:52 +0800

    okay, maybe not

--- fetch.js
+++ fetch.js
@@ -291,10 +291,6 @@
           reject(new TypeError('Network request failed'))
           return
         }
-        if (status >= 400) {
-          reject(new TypeError(xhr.statusText))
-          return
-        }
         var options = {
           status: status,
           statusText: xhr.statusText,
